2ebe87df6744b395713e185dc0ac7ca099379cda
alx787 <kaanova@gmail.com>
2018-10-10 02:52:35 +0300

    some ajax added

--- src/main/java/ru/hlynov/oit/alex/jira/webwork/GroupBossesSettingsAction.java
+++ src/main/java/ru/hlynov/oit/alex/jira/webwork/GroupBossesSettingsAction.java
@@ -1,17 +1,35 @@
 package ru.hlynov.oit.alex.jira.webwork;
 
+import com.atlassian.crowd.embedded.api.Group;
+import com.atlassian.jira.bc.group.search.GroupPickerSearchService;
+import com.atlassian.jira.component.ComponentAccessor;
+//import com.atlassian.jira.user.util.UserManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.atlassian.jira.web.action.JiraWebActionSupport;
 
+import java.util.List;
+
 public class GroupBossesSettingsAction extends JiraWebActionSupport
 {
     private static final Logger log = LoggerFactory.getLogger(GroupBossesSettingsAction.class);
+
+    private List<Group> allGroups;
 
     @Override
     public String execute() throws Exception {
 
         super.execute(); //returns SUCCESS
         return SUCCESS;
+    }
+
+    public List<Group> getAllGroups() {
+        GroupPickerSearchService groupSearch = ComponentAccessor.getComponent(GroupPickerSearchService.class);
+        List<Group> groupList = groupSearch.findGroups("");
+
+
+//        UserManager userManager = ComponentAccessor.getUserManager();
+//        Set<Group> groupSet = userManager.getAllGroups();
+        return groupList;
     }
 }
--- src/main/resources/js/groupbosses.js
+++ src/main/resources/js/groupbosses.js
@@ -18,4 +18,49 @@ AJS.$(function() {
     });
 
 
+    // инициализация селектов
+    // группы
+    AJS.$("#podr-field").auiSelect2();
+    AJS.$("#podr-field").on("change", function(e){
+        setOptionsForUsersSelect(AJS.$(this).select2("data").text);
+    });
+    // сотрудники
+    AJS.$("#boss-field").auiSelect2();
+
+    // var data = [ {"id": 1, "text": "Option 1"}, {"id": 2, "text": "Option 2"}, {"id": 2, "text": "Option 2"} ];
+    // AJS.$("#boss-field").select2({
+    //     data: data
+    // })
+
 })
+
+function setOptionsForUsersSelect(groupName) {
+    //alert(groupName);
+
+    AJS.$.ajax({
+        url: AJS.params.baseURL + "/rest/api/2/group/member?groupname=" + groupName,
+        type: "get",
+//        data: ({projectsOnly : "true"}),
+        dataType: "json",
+        success: function(data){
+            var arrJSON = [];
+            for (var i = 0; i < data.total; i++) {
+                var objJSON = {};
+                objJSON.id = data.values[i].name;
+                objJSON.text = data.values[i].displayName;
+                arrJSON.push(objJSON);
+            };
+
+            console.log(JSON.stringify(arrJSON));
+
+            var jsonText = JSON.stringify(arrJSON);
+            AJS.$("#boss-field").auiSelect2({
+                data: jsonText
+                // results: function (data, page) {
+                //             return {
+                //                 results: data
+                //             };
+            });
+        }
+    });
+}
